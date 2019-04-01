db90a91920f8b62400b71bf0cdc20d439509b6f9
Chris Solinas <solinas@ualberta.ca>
2019-04-01 10:17:57 -0600

    add nodes / sec instrumentation

--- src/solver.h
+++ src/solver.h
@@ -1,7 +1,11 @@
 // Copyright 2019 Chris Solinas
 #pragma once
 
+#include<chrono>
 #include "Go.h"
+
+typedef std::chrono::system_clock Clock;
+typedef std::chrono::duration<float> float_seconds;
 
 struct Result {
   Result() : value(0), best_move(-1) {}
@@ -12,9 +16,10 @@ struct Result {
 class Solver {
  private:
   int nodes;
+  bool verbose;
   Result alpha_beta(Go *game, float alpha, float beta, int depth);
 
  public:
-  Solver() : nodes(0) {}
+  Solver() : nodes(0), verbose(true) {}
   float solve(Go *game);
 };
